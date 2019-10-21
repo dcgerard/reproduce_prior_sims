c5c3b55d155175a099a4c96d50542cc3bd4b98aa
David Gerard <gerard.1787@gmail.com>
2019-10-21 14:05:21 -0400

    figure text for clarity

--- code/plot_sims.R
+++ code/plot_sims.R
@@ -8,13 +8,15 @@ library(ggthemes)
 sims <- as_tibble(readRDS("./output/prior_sims/sims_out.RDS"))
 
 ## Format sims for tidy data --------------------------------------------------
-factor_order_vec <- c("Uniform",
-                      "F1",
-                      "Hardy-Weinberg",
-                      "Beta-binomial",
-                      "Normal",
-                      "Unimodal",
-                      "General")
+factor_order_vec<- c("Uniform",
+                     "F1",
+                     "Hardy-Weinberg",
+                     "Beta-binomial",
+                     "Normal",
+                     "Unimodal",
+                     "General")
+
+factor_order_geno <- str_c("Genotype Distribution\n", factor_order_vec)
 
 sims %>%
   select(-ploidy, -seq) %>%
@@ -32,6 +34,7 @@ sims %>%
                             "f1"      = "F1",
                             "flex"    = "General",
                             "uniform" = "Uniform"),
+         geno_dist = str_c("Genotype Distribution\n", geno_dist),
          Prior = recode(Prior,
                         "hw"      = "Hardy-Weinberg",
                         "bb"      = "Beta-binomial",
@@ -40,13 +43,14 @@ sims %>%
                         "f1"      = "F1",
                         "flex"    = "General",
                         "uniform" = "Uniform"),
-         geno_dist = parse_factor(geno_dist, levels = factor_order_vec),
+         geno_dist = parse_factor(geno_dist, levels = factor_order_geno),
          Prior = parse_factor(Prior, levels = factor_order_vec)) ->
   simslong
 
 ## Plot Proportion correct ----------------------------------------------------
 for (index in seq_along(factor_order_vec)) {
-  current_geno_dist <- factor_order_vec[index]
+  current_geno_dist <- factor_order_geno[index]
+  current_geno_val <- factor_order_vec[index]
   simslong %>%
     filter(geno_dist == current_geno_dist) %>%
     ggplot(aes(x = Prior, y = pc, color = issame)) +
@@ -57,10 +61,10 @@ for (index in seq_along(factor_order_vec)) {
     theme_bw() +
     theme(strip.background = element_rect(fill = "white"),
           axis.text.x = element_text(angle = 45, hjust = 1)) +
-    xlab("Assumed Prior") +
+    xlab("Assumed Prior During Fit") +
     ylab("Proportion Correct") ->
     pl
-  ggsave(filename = paste0("./output/figures/pc_plots/pc_", current_geno_dist, ".pdf"),
+  ggsave(filename = paste0("./output/figures/pc_plots/pc_", current_geno_val, ".pdf"),
          plot = pl,
          height = 7,
          width = 6,
@@ -73,7 +77,8 @@ simslong %>%
   distinct() ->
   smalldf
 for (index in seq_along(factor_order_vec)) {
-  current_geno_dist <- factor_order_vec[index]
+  current_geno_dist <- factor_order_geno[index]
+  current_geno_val <- factor_order_vec[index]
   simslong %>%
     filter(geno_dist == current_geno_dist) %>%
     ggplot(aes(x = Prior, y = bias, color = issame)) +
@@ -84,12 +89,12 @@ for (index in seq_along(factor_order_vec)) {
     theme_bw() +
     theme(strip.background = element_rect(fill = "white"),
           axis.text.x = element_text(angle = 45, hjust = 1)) +
-    xlab("Assumed Prior") +
+    xlab("Assumed Prior During Fit") +
     ylab("Estimated Bias") +
     geom_hline(data = smalldf, mapping = aes(yintercept = bias_real), alpha = 1/2, lwd = 1, col = "blue") +
     scale_y_log10() ->
     pl
-  ggsave(filename = paste0("./output/figures/bias_plots/bias_", current_geno_dist, ".pdf"),
+  ggsave(filename = paste0("./output/figures/bias_plots/bias_", current_geno_val, ".pdf"),
          plot = pl,
          height = 7,
          width = 6,
@@ -103,7 +108,8 @@ logit_fun <- function(x) {
   log(x / (1 - x))
 }
 for (index in seq_along(factor_order_vec)) {
-  current_geno_dist <- factor_order_vec[index]
+  current_geno_dist <- factor_order_geno[index]
+  current_geno_val <- factor_order_vec[index]
   simslong %>%
     filter(geno_dist == current_geno_dist) %>%
     mutate(logit_od = logit_fun(od)) %>%
@@ -115,11 +121,11 @@ for (index in seq_along(factor_order_vec)) {
     theme_bw() +
     theme(strip.background = element_rect(fill = "white"),
           axis.text.x = element_text(angle = 45, hjust = 1)) +
-    xlab("Assumed Prior") +
+    xlab("Assumed Prior During Fit") +
     ylab("logit(Overdispersion Estimate)") +
     geom_hline(data = smalldf, mapping = aes(yintercept = logit_fun(od_real)), alpha = 1/2, lwd = 1, col = "blue") ->
     pl
-  ggsave(filename = paste0("./output/figures/od_plots/od_", current_geno_dist, ".pdf"),
+  ggsave(filename = paste0("./output/figures/od_plots/od_", current_geno_val, ".pdf"),
          plot = pl,
          height = 7,
          width = 6,
@@ -132,7 +138,8 @@ simslong %>%
          diff = pm - epm) ->
   simslong
 for (index in seq_along(factor_order_vec)) {
-  current_geno_dist <- factor_order_vec[index]
+  current_geno_dist <- factor_order_geno[index]
+  current_geno_val <- factor_order_vec[index]
   simslong %>%
     filter(geno_dist == current_geno_dist) %>%
     ggplot(aes(x = Prior, y = diff, color = issame)) +
@@ -143,18 +150,16 @@ for (index in seq_along(factor_order_vec)) {
     theme_bw() +
     theme(strip.background = element_rect(fill = "white"),
           axis.text.x = element_text(angle = 45, hjust = 1)) +
-    xlab("Assumed Prior") +
+    xlab("Assumed Prior During Fit") +
     ylab("Proportion Missed - Estimated Proportion Missed") +
     geom_hline(yintercept = 0, alpha = 1/2, lwd = 1, col = "blue") ->
     pl
-  ggsave(filename = paste0("./output/figures/epm_plots/epm_", current_geno_dist, ".pdf"),
+  ggsave(filename = paste0("./output/figures/epm_plots/epm_", current_geno_val, ".pdf"),
          plot = pl,
          height = 7,
          width = 6,
          family = "Times")
 }
-
-
 
 
 ###############################
@@ -172,7 +177,7 @@ simslong %>%
   theme_bw() +
   theme(strip.background = element_rect(fill = "white"),
         axis.text.x = element_text(angle = 60, hjust = 1)) +
-  xlab("Assumed Prior") +
+  xlab("Assumed Prior During Fit") +
   ylab("Proportion Correct") ->
   pl
 
@@ -192,7 +197,7 @@ simslong %>%
   theme_bw() +
   theme(strip.background = element_rect(fill = "white"),
         axis.text.x = element_text(angle = 60, hjust = 1)) +
-  xlab("Assumed Prior") +
+  xlab("Assumed Prior During Fit") +
   ylab("Proportion Missed - Estimated Proportion Missed") +
   geom_hline(yintercept = 0, alpha = 1/2, lwd = 1, col = "blue") ->
   pl
